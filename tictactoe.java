884796c547692e155457f2f6f64fb52fbc91ef7d
Sooryan K R <sooryankr@gmail.com>
2023-09-18 17:59:47 +0530

    **

---
+++ tictactoe.java
@@ -0,0 +1,324 @@
+import java.util.Scanner;
+
+public class tictactoe {
+    public void tictactoe1(){
+        Scanner read = new Scanner(System.in);
+          int i,j,b;
+          
+        char board [][] = new char [3][3];
+        int s[][] = new int [3][3];
+        int a=0;
+        for (i=0;i<3;i++){
+            for( j =0;j<3;j++){
+                
+                s[i][j]=a;
+                a++;
+        
+            }
+        }
+
+
+        for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+s[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+    
+
+    
+        System.out.println("O's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'O';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+    
+
+          System.out.println("X's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'X';
+                    break;
+                }
+                
+            }
+        
+        }
+    
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+  
+        System.out.println("O's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'O';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         
+
+
+
+
+        
+
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+     
+
+
+          System.out.println("X's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'X';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         
+
+
+
+
+
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+       
+   
+        System.out.println("O's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'O';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         
+
+
+
+
+
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+     
+
+
+
+          System.out.println("X's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'X';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         
+
+
+
+
+
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+
+      
+        System.out.println("O's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'O';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         
+
+
+
+
+
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+     
+      
+
+
+
+          System.out.println("X's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'X';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         
+
+
+
+
+
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+        
+    
+
+     System.out.println("O's move");
+        System.out.println("enter the position");
+     b = read.nextInt();
+
+      for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                if(s[i][j]==b){
+                    board[i][j]=  'O';
+                    break;
+                }
+                
+            }
+        
+        }
+         
+         
+
+
+
+
+
+
+         for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                System.out.print("|_"+""+board[i][j]+ ""+"_|");
+            }
+            System.out.println();
+        }
+        
+
+    }
+    
+    }
